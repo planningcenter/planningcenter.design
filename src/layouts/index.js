d7838887d5eec145cd817546a6c63fd084005ffb
Dustin Secrest <dustin.secrest@gmail.com>
2018-03-27 11:02:01 -0700

    nav

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,24 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Helmet from "react-helmet";
-
+import { PageList } from "../components/all_site_page_list";
 import Header from "../components/Header";
+import Nav from "../components/Nav";
 import "../styles/shared.css";
 import "../styles/index.css";
 
+
 const TemplateWrapper = ({ children }) => (
-  <div className="Wrapper">
+  <div className="Wrapper" style={{ alignItems: "stretch", display: "flex", minHeight: "100vh" }}>
     <Helmet
-      title="Planning Center Dot Design"
+      title="Planning Center Design"
       meta={[
         { name: "description", content: "Sample" },
         { name: "keywords", content: "sample, something" },
       ]}
     />
-    <Header />
-    <main className="Main">
-      {children()}
-    </main>
+    <Nav/>
+    {children()}
   </div>
 );
 
